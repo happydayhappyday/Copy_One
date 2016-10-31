c7f5f423960e05b5956a6900322073c9d1c49484
xiayuanqun <xiayuanqun@xiayuanqundemacbook-pro.local>
2016-10-31 13:10:20 +0800

    pull

--- 一个/常量文件/OneConst.h
+++ 一个/常量文件/OneConst.h
@@ -9,6 +9,8 @@
 #import <Foundation/Foundation.h>
 #import "OneHttpConst.h"
 
+
+
 #define ONE_SCREEN_WIDTH ([UIScreen mainScreen].bounds.size.width)
 #define ONE_SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
 #define ONEColor(r,g,b,a) [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:a]
