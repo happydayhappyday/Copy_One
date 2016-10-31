fca3f7d1bcd590f69bf0a3db45ba5570bb98bc22
xiayuanqun <xiayuanqun@xiayuanqundemacbook-pro.local>
2016-10-31 13:15:55 +0800

    个人信息界面修改

---
+++ 一个/view/usertableView/UserTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  UserTableViewCell.h
+//  一个
+//
+//  Created by xiayuanqun on 16/10/31.
+//  Copyright © 2016年 xiayuanqun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserTableViewCell : UITableViewCell
+
+@end
---
+++ 一个/view/usertableView/UserTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  UserTableViewCell.m
+//  一个
+//
+//  Created by xiayuanqun on 16/10/31.
+//  Copyright © 2016年 xiayuanqun. All rights reserved.
+//
+
+#import "UserTableViewCell.h"
+
+@implementation UserTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
